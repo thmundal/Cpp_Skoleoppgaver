11395e658a6ea7d0d804d9981798020a6c1a0407
Thomas Mundal <thmundal@gmail.com>
2017-10-23 15:39:29 +0200

    overloaded plus operator to add 2 lines to become a polygon, or add two polygons to become one

--- BibliotekOppgave/Polygon.cpp
+++ BibliotekOppgave/Polygon.cpp
@@ -3,7 +3,6 @@
 
 namespace poly {
 	Polygon::Polygon() {
-
 	}
 
 	Polygon::Polygon(Point* _points) {
@@ -26,10 +25,11 @@ namespace poly {
 	}
 
 	void Polygon::output() {
-		std::cout << "Et polygon bygd opp av: ";
+		std::cout << "Et polygon bygd opp av: " << std::endl;
 		for (int i = 0; i < num_points; i++) {
 			points[i].output();
 		}
+		std::cout << std::endl;
 	}
 
 	Polygon operator+(Line& a, Point& b) {
@@ -43,4 +43,36 @@ namespace poly {
 		return out;
 	}
 
+	Polygon operator+(Line& a, Line& b) {
+		Point* points = new Point[4];
+		points[0] = a.points[0];
+		points[1] = a.points[1];
+		points[2] = b.points[0];
+		points[3] = b.points[1];
+
+		Polygon out = Polygon(points);
+		out.num_points = 4;
+		return out;
+	}
+
+	Polygon Polygon::operator+(Polygon& b) {
+		Point* points = new Point[this->num_points + b.num_points];
+
+		for (int i = 0; i < (this->num_points + b.num_points); i++) {
+			if (i < this->num_points) {
+				points[i] = this->points[i];
+			}
+			else {
+				points[i] = b.points[i - this->num_points];
+			}
+		}
+
+		Polygon out = Polygon(points);
+		out.num_points = (this->num_points + b.num_points);
+		return out;
+	}
+
+	Polygon::~Polygon() {
+		delete[] points;
+	}
 }
--- BibliotekOppgave/Polygon.h
+++ BibliotekOppgave/Polygon.h
@@ -9,7 +9,10 @@ namespace poly {
 		Polygon();
 		friend Polygon* operator+(Polygon& a, Point& p);
 		friend Polygon operator+(Line& a, Point& b);
+		friend Polygon operator+(Line& a, Line&b);
+		Polygon operator+(Polygon& b);
 		void output();
+		~Polygon();
 	private:
 		int num_points = 2;
 		Point* points;
