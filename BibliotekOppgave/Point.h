9a98be5cc5b1bd5b144b873ec8c8dd1e1ac89c47
Thomas Mundal <thmundal@gmail.com>
2017-10-23 15:38:43 +0200

    Added support for 3d point

--- BibliotekOppgave/Point.h
+++ BibliotekOppgave/Point.h
@@ -4,9 +4,10 @@
 namespace poly {
 	class Point {
 	private:
-		int x, y;
+		int x = 0, y = 0, z = 0;
 	public:
-		Point(int, int);
+		Point(int x, int y);
+		Point(int x, int y, int z);
 		Point();
 		void output();
 	};
